93239d698a1ec647e18276aa7534e8295fe9ca20
Praharsh Singh <praharsh535@gmail.com>
2022-03-23 15:02:22 +0530

    Signup hashing + login controller

--- database/database.go
+++ database/database.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"notes-app/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var collection *mongo.Collection
@@ -36,13 +38,37 @@ func CreateDBInstance() {
 	fmt.Println("collection instance created")
 
 }
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 func InsertUser(user models.User) {
 
-	insertResult, err := collection.InsertOne(context.Background(), user)
+	HashedUserPassword, err := HashPassword(user.Password)
+	user.Password = HashedUserPassword
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	insertResult, err := collection.InsertOne(context.Background(), user)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("USER CREATED", insertResult.InsertedID)
+}
+func CheckUserLogin(user models.User) {
+	var result models.User
+	err := collection.FindOne(context.Background(), bson.D{{"username", user.Username}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		log.Fatal(err)
+		return
+	}
+	// CheckPasswordHash(user.Password, result[fmt.Sprint("password")])
+	fmt.Println("found document ", result)
+	fmt.Println("found values " + result.Username + " " + result.Password)
 }
--- main.go
+++ main.go
@@ -22,15 +22,31 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	json.NewDecoder(r.Body).Decode(&user)
 	fmt.Println("create hit with user", user)
+	// this function is called two times on its own , so returning out from the first request having empty data
+	if user.Username == "" {
+		return
+	}
 	database.InsertUser(user)
+	json.NewEncoder(w).Encode(user)
 }
-func doNothing(w http.ResponseWriter, r *http.Request) {}
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	fmt.Println("login hit with user", user)
+
+	database.CheckUserLogin(user)
+	// json.NewEncoder(w).Encode(loggedUser)
+}
 
 func main() {
 	router := mux.NewRouter()
 	API_BASE_URL := "/go-api"
 	database.CreateDBInstance()
-	router.HandleFunc("/favicon.ico", doNothing)
 	router.HandleFunc(API_BASE_URL+"/user/add", CreateUser).Methods("POST", "OPTIONS")
+	router.HandleFunc(API_BASE_URL+"/user/login", LoginUser).Methods("POST", "OPTIONS")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8000", router))
 }
